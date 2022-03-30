429031b3fde3385a73aa2f9feebc1679f5a54e1d
tim <tim@mw3.ch>
2022-03-30 18:31:40 +0200

    styled popup and added clickevents

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -1,23 +1,39 @@
-import React from "react";
+import classNames from "classnames";
+import React, { SyntheticEvent } from "react";
 import { errorResolveSubject } from "../utils/subjects";
 import FlexContainer from "./FlexContainer";
 import Heading from "./Heading";
 import MaterialIconButton from "./MaterialIconButton";
 import "./Popup.css";
 import Tile from "./Tile";
+
 /**
  * A component that can be used to display a popup.
  * TODO: style it with CSS and make it responsive
  */
-class Popup extends React.Component<{ value: any; popupId: any }, {}> {
+class Popup extends React.Component<
+    { value: any; popupId: any },
+    { hidden: boolean }
+> {
     constructor(props: any) {
         super(props);
         // bind the resolve function to the component
         this.resolve = this.resolve.bind(this);
+        this.state = {
+            hidden: true,
+        };
     }
-    resolve() {
-        // resolve the error
-        errorResolveSubject.next(this.props.popupId);
+    componentDidMount() {
+        // show the popup after a delay to have a nice animation
+        setTimeout(() => {
+            this.setState({ hidden: false });
+        }, 20);
+    }
+    resolve(e: SyntheticEvent) {
+        // if the target is the closeButton or the container around the popup, resolve the error
+        if (e.target === e.currentTarget) {
+            errorResolveSubject.next(this.props.popupId);
+        }
     }
     render(): React.ReactNode {
         if (this.props.value)
@@ -27,9 +43,16 @@ class Popup extends React.Component<{ value: any; popupId: any }, {}> {
                     direction="row"
                     verticalCenter
                     horizontalCenter
-                    className="Popup"
+                    className={classNames("Popup", {
+                        "Popup-Hidden": this.state.hidden,
+                    })}
+                    onClick={this.resolve}
                 >
-                    <Tile>
+                    <Tile
+                        className={classNames("Popup-Tile", {
+                            "Popup-Tile-Hidden": this.state.hidden,
+                        })}
+                    >
                         {/* the heading */}
                         <Heading level={2}>Error</Heading>
                         {/* the close button */}
@@ -38,7 +61,9 @@ class Popup extends React.Component<{ value: any; popupId: any }, {}> {
                             action={this.resolve}
                         />
                         {/* the error message */}
-                        <span>{this.props.value}</span>
+                        <span className="Popup-Message">
+                            {this.props.value}
+                        </span>
                     </Tile>
                 </FlexContainer>
             );
